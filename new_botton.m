c1ed602b6b255e7c3db658768210ceef25fcc4dc
39216 <39216@39216-445824.TELXSIDS.NET>
2024-03-21 10:57:49 +0530

    green

--- new_botton.m
+++ new_botton.m
@@ -4,5 +4,5 @@ btn1 = uibutton(fig,"Position",[100 100 100 20]);
 btn2 = uibutton(fig,"Position",[100 75 100 20]);
 btn3 = uibutton(fig,"Position",[100 50 100 20]);
 btns = [btn1 btn2];
-set(btns,"FontColor","blue")
+set(btns,"FontColor","green")
 set(btn3,"FontColor","red")
