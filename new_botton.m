0518355ebbd8a8b9da90daea139d0ca4efa47a7a
39216 <39216@39216-445824.TELXSIDS.NET>
2024-03-21 10:54:25 +0530

    new file created

---
+++ new_botton.m
@@ -0,0 +1,8 @@
+% new code
+fig = uifigure;
+btn1 = uibutton(fig,"Position",[100 100 100 20]);
+btn2 = uibutton(fig,"Position",[100 75 100 20]);
+btn3 = uibutton(fig,"Position",[100 50 100 20]);
+btns = [btn1 btn2];
+set(btns,"FontColor","blue")
+set(btn3,"FontColor","red")
